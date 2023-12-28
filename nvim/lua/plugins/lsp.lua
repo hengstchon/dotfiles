932c985da98b1da792d9d22c9d2b2152510a5999
hengstchon <hengstchon@gmail.com>
2023-12-28 12:29:51 +0100

    nvim: reorganize

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -116,6 +116,7 @@ return {
             end
 
             -- Fallback to system Python.
+            local exepath = vim.fn.exepath
             return exepath("python3") or exepath("python") or "python"
           end
 
@@ -172,95 +173,6 @@ return {
             },
           })
         end,
-      })
-    end,
-  },
-
-  -- linting
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local lint = require("lint")
-
-      lint.linters_by_ft = {
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-      }
-
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
-
-  -- formatting
-  {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    keys = {
-      {
-        "<leader>fm",
-        function()
-          require("conform").format({ lsp_fallback = true })
-        end,
-        mode = "",
-        desc = "Format buffer",
-      },
-    },
-    config = function()
-      local conform = require("conform")
-
-      conform.setup({
-        formatters_by_ft = {
-          javascript = { "prettierd" },
-          typescript = { "prettierd" },
-          javascriptreact = { "prettierd" },
-          typescriptreact = { "prettierd" },
-          markdown = { "prettierd" },
-          lua = { "stylua" },
-          -- Conform will run multiple formatters sequentially
-          python = { "isort", "black" },
-        },
-        format_on_save = function(bufnr)
-          -- Disable autoformat on certain filetypes
-          local ignore_filetypes = { "php", "sql", "java" }
-          if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-            return
-          end
-          -- Disable with a global or buffer-local variable
-          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-          end
-          -- Disable autoformat for files in a certain path
-          local bufname = vim.api.nvim_buf_get_name(bufnr)
-          if bufname:match("/node_modules/") then
-            return
-          end
-          return { lsp_fallback = true }
-        end,
-      })
-
-      vim.api.nvim_create_user_command("FormatDisable", function(args)
-        if args.bang then
-          -- FormatDisable! will disable formatting just for this buffer
-          vim.b.disable_autoformat = true
-        else
-          vim.g.disable_autoformat = true
-        end
-      end, {
-        desc = "Disable autoformat-on-save",
-        bang = true,
-      })
-      vim.api.nvim_create_user_command("FormatEnable", function()
-        vim.b.disable_autoformat = false
-        vim.g.disable_autoformat = false
-      end, {
-        desc = "Re-enable autoformat-on-save",
       })
     end,
   },
