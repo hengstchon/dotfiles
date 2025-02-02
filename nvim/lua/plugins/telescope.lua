b81de7b2f21358f332bf258af61703003ae30f26
hengstchon <hengstchon@gmail.com>
2025-02-03 00:17:24 +0100

    nvim: telescope: remove esc keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,14 +10,12 @@ return {
     config = function()
       local telescope = require("telescope")
       local builtin = require("telescope.builtin")
-      local actions = require("telescope.actions")
       local actions_layout = require("telescope.actions.layout")
 
       telescope.setup({
         defaults = {
           mappings = {
             i = {
-              ["<esc>"] = actions.close,
               ["<C-y>"] = actions_layout.toggle_preview,
             },
           },
