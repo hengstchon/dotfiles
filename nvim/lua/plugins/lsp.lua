02cfb7d1d7f56a378d657b1bce840537afdfa639
hengstchon <hengstchon@gmail.com>
2025-03-29 08:58:16 +0100

    nvim: update diagnostic config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -38,7 +38,7 @@ return {
       })
 
       local opts = { noremap = true, silent = true }
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+      -- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
       -- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
       -- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
       vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
@@ -48,7 +48,7 @@ return {
         severity_sort = true,
         float = {
           -- from nvim_open_win()
-          focusable = false,
+          focusable = true,
           border = "rounded",
           -- from vim.diagnostic.open_float()
           header = "",       -- default: Diagnostics
